1f0c315d637cb78b5b8e0054f29adc56c75224bb
M. Fadhil Alvan Syah <80678865+mfaskdr10@users.noreply.github.com>
2025-01-28 20:34:21 +0700

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{php,js}", "index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
